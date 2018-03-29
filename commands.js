38c41307980fe526613067a5ac4ff9d4c2c626e8
Gabe Martin <gabema@gmail.com>
2018-03-29 14:15:13 -0700

    Adding the ability to update uris

--- commands.js
+++ commands.js
@@ -1,17 +1,37 @@
 const Promise = require('bluebird');
 const columnify = require('columnify');
 const {addFunction, listCommands} = require('./parser');
+const {setAPIv1Env} = require('@ercorp/er-api-js/apiv1/v1Services');
+const {setAuthEnv} = require('@ercorp/er-api-js/auth/authServices');
 
 const addGeneralFunctions = (rl) => {
     addFunction({
         command: 'QUIT',
-        description: 'Exits',
+        description: 'Exits the application',
         cb: () => Promise
             .resolve()
             .then(() => {
                 console.log('Exiting.... goodbye!');
                 rl.close();
                 return 'QUIT';
+            })
+    });
+
+    addFunction({
+        command: 'SetUris',
+        description: 'Sets the uris used by ER Auth and API calls. Format: "<API_URL> <AUTH_URL>"',
+        cmdRegEx: /^(\S*) (\S*)$/,
+        cb: (params) => Promise
+            .resolve()
+            .then(() => {
+                const uris = {
+                    api: params[1],
+                    auth: params[2]
+                };
+                console.log(uris);
+                setAPIv1Env({url: uris.api});
+                setAuthEnv({url: uris.auth});
+                return uris;
             })
     });
 
