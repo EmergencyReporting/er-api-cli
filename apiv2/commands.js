ba5df47922b803d8f152b2546e37388181dce0e1
Gabe Martin <gabema@gmail.com>
2018-03-31 20:48:49 -0700

    V 0.4 (#7)
    * Adding events and preliminary inspection commands
    * updating inspection output
    * adding additional events and auth commands

--- apiv2/commands.js
+++ apiv2/commands.js
@@ -1,9 +1,12 @@
 const reduce = require('lodash.reduce');
 const {addFunction} = require('../parser');
 const {getRosters} = require('@ercorp/er-api-js/apiv2/rosters');
+const {getInspections} = require('@ercorp/er-api-js/apiv2/inspections');
 const columnify = require('columnify');
+const {addV2EventFunctions} = require('./eventCommands');
 
 const addV2Functions = () => {
+    addV2EventFunctions();
     addFunction({
         command: 'v2Rosters',
         cmdRegEx: /^(\d*)\s?(.*)$/,
@@ -42,6 +45,60 @@ const addV2Functions = () => {
                     console.log('No Rosters returned.');
                 };
                 return data;
+            });
+        }
+    });
+    addFunction({
+        command: 'v2Inspections',
+        cmdRegEx: /^(.*)$/,
+        description: 'Gets a list of Inspections. Uses the optional format offset|limit|filter|orderby' +
+                '|rowVersionDefaults to 5 events.',
+        cb: params => {
+            const splitParams = (params[1] || '').split('|');
+            let queryParams = {};
+            if (splitParams[0]) {
+                queryParams.offset = splitParams[0];
+            }
+            queryParams.limit = parseInt(splitParams[1] || '5', 10);
+
+            if (splitParams[2]) {
+                queryParams.filter = splitParams[2];
+            }
+            if (splitParams[3]) {
+                queryParams.orderby = splitParams[3];
+            }
+            if (splitParams[4]) {
+                queryParams.rowVersion = splitParams[4];
+            }
+
+            return getInspections(queryParams).then(data => {
+                if (data.inspections) {
+                    const inspectionSummaries = reduce(data.inspections, (acc, {
+                        inspectionID,
+                        inspectorUserID,
+                        occupancyID,
+                        inspectionDate,
+                        locked,
+                        inspectionType,
+                        formID,
+                        rowVersion
+                    }) => {
+                        acc.push({
+                            inspectionID,
+                            inspectorUserID,
+                            occupancyID,
+                            inspectionDate,
+                            locked,
+                            inspectionType,
+                            formID,
+                            rowVersion
+                        });
+                        return acc;
+                    }, []);
+                    console.log(columnify(inspectionSummaries));
+                } else {
+                    console.log('No inspections returned.');
+                };
             });
         }
     });
