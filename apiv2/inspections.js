419d77da4792afb34c5b18de9d2a71d72f08f4a7
Gabe Martin <gabema@gmail.com>
2018-04-13 22:10:25 -0700

    adding roster commands and updating inspections

--- apiv2/inspections.js
+++ apiv2/inspections.js
@@ -1,6 +1,7 @@
 const {addFunction} = require('../parser');
 const {getInspections} = require('@ercorp/er-api-js/apiv2/inspections');
 const columnify = require('columnify');
+const {splitParams, addParamIfPresent} = require('../util');
 
 const addV2Inspections = () => {
     addFunction({
@@ -9,22 +10,13 @@ const addV2Inspections = () => {
         description: 'Gets a list of Inspections. Uses the optional format offset|limit|filter|orderby' +
                 '|rowVersionDefaults to 5 events.',
         cb: params => {
-            const splitParams = (params[1] || '').split('|');
+            const sp = splitParams(params[1]);
             let queryParams = {};
-            if (splitParams[0]) {
-                queryParams.offset = splitParams[0];
-            }
-            queryParams.limit = parseInt(splitParams[1] || '5', 10);
-
-            if (splitParams[2]) {
-                queryParams.filter = splitParams[2];
-            }
-            if (splitParams[3]) {
-                queryParams.orderby = splitParams[3];
-            }
-            if (splitParams[4]) {
-                queryParams.rowVersion = splitParams[4];
-            }
+            addParamIfPresent(queryParams, sp, 'offset', 0);
+            queryParams.limit = parseInt(sp[1] || '5', 10);
+            addParamIfPresent(queryParams, sp, 'filter', 2);
+            addParamIfPresent(queryParams, sp, 'orderby', 3);
+            addParamIfPresent(queryParams, sp, 'rowVersion', 4);
 
             return getInspections(queryParams).then(data => {
                 if (data.inspections && data.inspections.length) {
--- apiv2/rosters.js
+++ apiv2/rosters.js
@@ -1,6 +1,7 @@
 const {addFunction} = require('../parser');
-const {getRosters, createRoster} = require('@ercorp/er-api-js/apiv2/rosters');
+const {getRosters, createRoster, deleteRoster} = require('@ercorp/er-api-js/apiv2/rosters');
 const columnify = require('columnify');
+const {splitParams, addParamIfPresent} = require('../util');
 
 const addV2Rosters = () => {
     addFunction({
@@ -9,26 +10,47 @@ const addV2Rosters = () => {
         description: 'Gets a list of rosters. Optionally provide the number of rosters to get and a fi' +
                 'lter by clause. Defaults to 5 rosters without a filter.',
         cb: params => {
-            console.log(params[1]);
             const newRoster = JSON.parse(params[1]);
             return createRoster(newRoster).then(data => {
                 console.log(columnify(data));
+                return data;
+            }).catch(err => {
+                if (err.response.data) {
+                    console.log(`ERROR! Status code: ${err.response.status} - ${JSON.stringify(err.response.data)}`);
+                }
+                return err;
+            });
+        }
+    });
+    addFunction({
+        command: 'v2DeleteRoster',
+        cmdRegEx: /^(\d+)$/,
+        description: 'Deletes a roster. Must specify the rosterID to be deleted.',
+        cb: params => {
+            const rosterID = parseInt(params[1], 10);
+            return deleteRoster(rosterID).catch(err => {
+                if (err.response.data) {
+                    console.log(`ERROR! Status code: ${err.response.status} - ${JSON.stringify(err.response.data)}`);
+                }
+                return err;
             });
         }
     });
     addFunction({
         command: 'v2Rosters',
-        cmdRegEx: /^(\d*)\s?(.*)$/,
+        cmdRegEx: /^(.*)$/,
         description: 'Gets a list of rosters. Optionally provide the number of rosters to get and a fi' +
-                'lter by clause. Defaults to 5 rosters without a filter.',
+                'lter by clause.  Uses the optional format offset|limit|filter|orderby|rowVersion' +
+                'Defaults to 5 rosters.',
         cb: params => {
-            const limit = parseInt(params[1] || '5', 10);
-            let queryParams = {
-                limit
-            };
-            if (params[2]) {
-                queryParams.filter = params[2];
-            }
+            const sp = splitParams(params[1]);
+            let queryParams = {};
+            addParamIfPresent(queryParams, sp, 'offset', 0);
+            queryParams.limit = parseInt(sp[1] || '5', 10);
+            addParamIfPresent(queryParams, sp, 'filter', 2);
+            addParamIfPresent(queryParams, sp, 'orderby', 3);
+            addParamIfPresent(queryParams, sp, 'rowVersion', 4);
+
             return getRosters(queryParams).then(data => {
                 if (data.rosters && data.rosters.length) {
                     const rosterSummaries = data
